c9779180bed07495f61aa658e898671cfc38f50d
cyh128 <69246156+15dd@users.noreply.github.com>
2023-01-05 12:22:04 +0800

    update

--- README.md
+++ README.md
@@ -1,53 +1,28 @@
-# File-Header-Forger
-绕过部分云盘的文件分享限制
-## 用途
-此工具针对使用部分云盘时,受到文件分享类型限制进行伪造
+# File Header Forger/文件伪造器
+- 使用Python编写的程序,GUI使用PyQt5
+- 该程序可用于绕过部分网盘的分享限制（如阿里云盘）
+
 ## 原理
-- aliyundrive在分享文件时,会查看文件头(即文件类型),假如检测到压缩文件等受到限制的文件时会无法分享,在对受限制的文件进行伪造文件头后,即可突破限制,正常分享
-- 如zip的文件头为504b0304,将其改为aliyundrive支持的格式即可
+经本人测试（2023.1)，阿里云盘对文件不光有后缀识别还有文件头识别（不确定，但修改它有效），所以光修改后缀没用，什么是文件头可以自行搜索。如下图，这是一张zip格式的文件的十六进制数据，选中的**504B0304**是zip格式的文件头，它可以说明这是一个zip格式的文件，所以我们想绕过检测的话，也要把它改了。
+![1](/img/1.png)
+我的做法是将首尾的4个字节对调，实现修改
+```
+with open(filePath,"rb+") as rawFile:
+    rawFile.seek(0,0)   #移动到文件的0(头)处偏移为0的位置
+    BOC = rawFile.read(4) #读取节                
+    rawFile.seek(-4,2) #移动到文件的2(尾)处偏移为-4(向前4个字节)的位置
+    EOC = rawFile.read(4
+    rawFile.seek(-4,2) 
+    rawFile.write(BOC) #向文件末尾的4个写成文件开头的4个字节
+    rawFile.seek(0,0)
+    rawFile.write(EOC) #向文件开头的4个写成文件末尾的4个
+    rawFile.close() #关闭文件
+```
+## 使用教程
+1. 将需要伪造的文件的路径填入框内（不要加双引号），支持选择文件或拖入文件 <br>
+仅支持伪造zip文件，如果你想伪造的文件不是zip格式，用压缩软件（如winrar）压缩成zip格式即可 
+![2](img/2.png)
 
-- ![image](1.jpg)
-
-- 经过多次测试,经伪造后再还原的文件与原文件对比,hash值始终相同,可放心使用(毕竟只是改了文件的前几个字节)
-
-- ![image](2.png)
-
-- 目前(2022/5/2)经过多次测试,aliyundrive只对文件头进行了限制,没对扩展名做限制
-## 伪造/还原格式
-- 目前已支持伪造jpg,png,zip,rar,psd,dll格式,其实可以自己去py文件添加相应的数据
-- 还原模式下无需手动添加,程序会根据后缀名自动恢复
- 
-- ![image](3.png)
-
-## 缺点
-使用麻烦,配置麻烦,会随之后的更新逐步完善操作体验
-## 参考内容:
-- https://blog.csdn.net/qq_40657585/article/details/83097386
-- https://blog.csdn.net/qq_36171645/article/details/88966127
-- https://qa.1r1g.com/sf/ask/3714342551/?lastactivity
-- https://www.cnblogs.com/schut/p/8410961.html
-- https://blog.51cto.com/u_15052689/2562469
-- https://blog.csdn.net/beijingyk/article/details/108581150
-- https://bluebird.blog.csdn.net/article/details/51122027
-- https://blog.csdn.net/qq_35441988/article/details/84994453
-- https://blog.csdn.net/weixin_40895135/article/details/121033002
-- https://www.jb51.net/article/143446.htm
-- https://blog.csdn.net/qq_40044912/article/details/122596325
-- http://www.wb86.com/post/308.html
-- https://www.bilibili.com/video/BV1134y1B7bo?p=2&spm_id_from=pageDriver
-- https://zhuanlan.zhihu.com/p/457429631
-- https://blog.csdn.net/weixin_29952383/article/details/113983844
-- https://blog.csdn.net/weixin_46348230/article/details/120684653
-- https://blog.csdn.net/qq_25921925/article/details/103987572
-- https://www.pythonheidong.com/blog/article/441185/3494dd82eeff9f34d613/
-- https://www.jianshu.com/p/2a931ce70b4b
-- https://www.cnpython.com/qa/412691
-- https://www.jianshu.com/p/d55e43c694db
-- https://blog.csdn.net/fan0829/article/details/119984383
-- https://www.pythonheidong.com/blog/article/190465/0049cea7f264404867fb/
-- https://www.5axxw.com/questions/content/29kxjs
-- https://zhuanlan.zhihu.com/p/162164360
-- https://zhuanlan.zhihu.com/p/338499426
----
-##### 2022/5/2 21:10 Created by cyh128
-
+2. 点击伪造或还原按钮，提示伪造成功，文件后缀自动改成.pdf，这时你的文件就可以骗过一些程序的文件类型识别
+3. 如需还原文件（即解压文件），将伪造的文件（pdf格式）拖入程序，点击伪造或还原按钮，提示还原成功
+   
---
+++ main.py
@@ -0,0 +1,155 @@
+import sys
+import re
+import os
+import webbrowser
+import binascii
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog, QWidget
+from ui_main import *
+from ui_about import *
+from ui_forge import *
+from ui_restore import *
+from PyQt5.QtCore import *
+
+
+
+#还原成功窗口===================================================================
+class restoreWindow(QWidget, Ui_restore):
+    def __init__(self):
+        super(restoreWindow, self).__init__()
+        self.setupUi(self)
+
+        self.setWindowModality(Qt.ApplicationModal)
+        self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)
+        self.setFixedSize(self.width(), self.height())
+#伪造成功窗口===================================================================
+class forgeWindow(QWidget, Ui_forge):
+    def __init__(self):
+        super(forgeWindow, self).__init__()
+        self.setupUi(self)
+
+        self.setWindowModality(Qt.ApplicationModal)
+        self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)
+        self.setFixedSize(self.width(), self.height())
+#关于本程序的窗口===============================================================
+class aboutWindow(QWidget, Ui_about):
+    def __init__(self):
+        super(aboutWindow, self).__init__()
+        self.setupUi(self)
+
+        self.setWindowModality(Qt.ApplicationModal)
+        self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)
+        self.setFixedSize(self.width(), self.height())
+#调出网页----------------------------------------------------------------------    
+    def openUrl(self):
+        webbrowser.open("https://github.com/15dd/File-Header-Forger", new=0, autoraise=True) 
+#主窗口========================================================================
+class MyWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setupUi(self)
+
+        self.setAcceptDrops(True)
+        self.setFixedSize(self.width(), self.height())       
+#负责文件拖放------------------------------------------------------------------
+    def dragEnterEvent(self, event): #鼠标拖入事件
+        event.accept()
+
+    def dropEvent(self, event): #鼠标松开事件
+        file = event.mimeData().urls()[0].toLocalFile() #获取拖入文件的路径
+        self.lineEdit.setText(file) #设置文本框内的内容为获取到的路径
+#关于Qt的窗口------------------------------------------------------------------
+    def aboutQt(self):
+        QMessageBox.aboutQt(self,title="")
+#选取文件窗口------------------------------------------------------------------
+    def selectFile(self):
+        filePath,fileType=QFileDialog.getOpenFileName(self,"选择文件","C:/","支持的文件(*zip *pdf);;zip压缩包(*.zip);;待还原的文件(*.pdf)")
+        self.lineEdit.setText(str(filePath))
+        self.lineEdit.setToolTip(str(filePath))
+#主程序入口--------------------------------------------------------------------
+    def Main(self):
+        filePath=self.getFilePath() #获取文件地址
+        if filePath == -1: #如果获取失败,结束程序
+            return
+        if str(os.path.splitext(filePath)[-1]) not in ['.zip','.pdf']: #判断文件后缀,如果不是支持的格式,结束程序
+            QMessageBox.warning(self,"警告","仅支持文件后缀为.zip或.pdf的文件")
+            return
+        self.core(filePath) #执行文件修改
+
+#从文本框内获取文件路径---------------------------------------------------------
+    def getFilePath(self):
+        try:
+            filePath=str(self.lineEdit.text()) #获取文本框内的内容
+            pattern = "^[a-zA-Z]:([\\\\/][^\\s\\\\/:*?<>\"|][^\\\\/:*?<>\"|]*)*([/\\\\])?$" #有效的windows路径表达式
+            reFP = re.match(pattern,filePath) #判断是否为有效的路径
+            if reFP == None: #如果不匹配,执行下列代码
+                QMessageBox.warning(self,"警告","文件路径格式错误,请输入有效的文件路径")
+                return -1
+            return filePath #如果匹配,返回获取到的内容
+        except:
+            QMessageBox.warning(self,"警告","文件路径获取异常,请检查文件路径")
+            return -1
+#文件读写----------------------------------------------------------------------
+    def core(self,filePath):
+        try:
+            with open(filePath,"rb+") as rawFile:
+                rawFile.seek(0,0)   #移动到文件的0(文件开头)处偏移为0的位置
+                BOC = rawFile.read(4) #读取4个字节                
+                rawFile.seek(-4,2) #移动到文件的2(文件末尾)处偏移为-4(向前4个字节)的位置
+                EOC = rawFile.read(4) 
+
+                rawFile.seek(-4,2) 
+                rawFile.write(BOC) #向文件末尾的4个字节改写成文件开头的4个字节
+                rawFile.seek(0,0)
+                rawFile.write(EOC) #向文件开头的4个字节改写成文件末尾的4个字节
+
+                rawFile.close() #关闭文件
+
+                if str(BOC) == str(self.HexToAscii(0x504B0304)):  #如果是zip文件,则伪造 
+                    oldname = os.path.splitext(filePath) 
+                    newname = oldname[0] + '.pdf'
+                    os.rename(filePath, newname)  #修改文件后缀
+
+                    self.lineEdit.setText(newname)
+
+                    QApplication.beep()
+                    forgeWin.show()
+                else: #如果不是zip文件,则还原
+                    oldname = os.path.splitext(filePath)
+                    newname = oldname[0] + '.zip'
+                    os.rename(filePath, newname) #修改文件后缀
+
+                    self.lineEdit.setText(newname)
+
+                    QApplication.beep()
+                    restoreWin.show()
+
+        except:
+            QMessageBox.critical(self,"错误","文件读写异常,请检查该文件\n可能的原因:\n1-文件不存在\n2-文件路径输错\n3-选择的不是文件,比如文件夹")
+#十六进制转ASCII---------------------------------------------------------------
+    def HexToAscii(self,n):
+        a = n
+        b = hex(a)            # 核心程序注释详见v1.1版本
+        b = b[2:]  
+        Ascii = binascii.a2b_hex(b) 
+        return Ascii
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myWin = MyWindow()
+    myWin.show()
+    aboutWin = aboutWindow()
+    forgeWin = forgeWindow()
+    restoreWin = restoreWindow()
+
+    myWin.actionb.triggered.connect(myWin.aboutQt)
+    myWin.actionabout.triggered.connect(aboutWin.show)
+    myWin.pushButton_2.clicked.connect(myWin.Main)
+    myWin.pushButton.clicked.connect(myWin.selectFile)
+    myWin.pushButton_3.clicked.connect(lambda:os.startfile("使用教程.pdf"))
+
+    aboutWin.pushButton.clicked.connect(aboutWin.openUrl)
+    
+    sys.exit(app.exec_())
